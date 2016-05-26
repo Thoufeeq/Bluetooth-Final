a4f28d1f115f597cd7d486232c4b32355af8e6f6
Thoufeeq <me@thoufeeq.com>
2016-05-26 09:59:29 +0530

    Reimplement Threading

--- java/com/example/bluetoothfinal/MainActivity.java
+++ java/com/example/bluetoothfinal/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.bluetoothfinal;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -12,26 +17,46 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     BluetoothAdapter mBluetoothAdapter;
     BluetoothDevice device;
     BluetoothSocket socket;
     InputStream inputstream;
-    String address = "20:13:10:08:09:19";
+    String address = "98:D3:31:40:9A:F7";
     public static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    static final int MESSAGE_READ = 1;
-    Button button;
+    private static final int DATA_RECEIVED = 1;
     TextView incoming;
+    byte[] buffer = new byte[1024];
+    int bytes;
+    String data;
+    String cmd1 = "Music Player turned ON";
+    String cmd2 = "Music Player turned OFF";
+    String cmd3 = "Command not recognized";
+    MediaPlayer mediaPlayer;
     Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            // get something from the thread (read incoming data)
-
+            switch (msg.what){
+                case 1:
+                    //update UI, in TextView
+                    data = Character.toString((char) bytes);
+                    if (data.equals("A")) {
+                        incoming.setText(cmd1);
+                        music_on();
+                    }
+                    else if (data.equals("B")) {
+                        incoming.setText(cmd2);
+                        music_off();
+                    }
+                    else {incoming.setText(cmd3);}
+            }
         }
     };
 
@@ -42,100 +67,65 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.b1);
         incoming = (TextView) findViewById(R.id.incoming);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        device = mBluetoothAdapter.getRemoteDevice(address);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                connect_device();
+                device_connect();
             }
         });
+    }
+
+    public void device_connect() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        device = mBluetoothAdapter.getRemoteDevice(address);
 
         ConnectThread mConnectThread = new ConnectThread(device);
         mConnectThread.start();
     }
 
-    public void connect_device() {
-        // perform some action
-        Toast.makeText(getApplicationContext(), "Button Clicked!", Toast.LENGTH_SHORT).show();
+    public void music_on() {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.song1);
+        mediaPlayer.start();
+    }
 
+    public void music_off() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"No music is being played!.",Toast.LENGTH_SHORT).show();
+        }
     }
 
     private class ConnectThread extends Thread {
-
         public ConnectThread(BluetoothDevice device) {
-            // Get a BluetoothSocket to connect with the given BluetoothDevice
             try {
                 socket = device.createRfcommSocketToServiceRecord(MY_UUID);
+                Toast.makeText(getApplicationContext(),"Connection Successful!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Listening for incoming data...",Toast.LENGTH_SHORT).show();
             } catch (IOException e) { }
         }
 
         public void run() {
-            // Cancel discovery because it will slow down the connection
             mBluetoothAdapter.cancelDiscovery();
-
             try {
-                // Connect the device through the socket. This will block
-                // until it succeeds or throws an exception
                 socket.connect();
+                inputstream = socket.getInputStream();
+                while (true) {
+                    bytes = inputstream.read();
+                    mHandler.obtainMessage(DATA_RECEIVED, bytes, -1, buffer)
+                            .sendToTarget();
+                }
             } catch (IOException connectException) {
-                // Unable to connect; close the socket and get out
                 try {
                     socket.close();
                 } catch (IOException closeException) { }
-                return;
             }
 
-            // Do work to manage the connection (in a separate thread)
-            ConnectedThread mConnectedThread = new ConnectedThread(socket);
-            mConnectedThread.start();
         }
 
-        /** Will cancel an in-progress connection, and close the socket */
-        public void cancel() {
-            try {
-                socket.close();
-            } catch (IOException e) { }
-        }
     }
-
-    private class ConnectedThread extends Thread {
-        public ConnectedThread(BluetoothSocket socket) {
-            // Get the input and output streams, using temp objects because
-            // member streams are final
-            try {
-                inputstream = socket.getInputStream();
-            } catch (IOException e) { }
-
-        }
-
-        public void run() {
-            byte[] buffer = new byte[1024];  // buffer store for the stream
-            int bytes; // bytes returned from read()
-
-            // Keep listening to the InputStream until an exception occurs
-            while (true) {
-                try {
-                    // Read from the InputStream
-                    bytes = inputstream.read(buffer);
-                    // Send the obtained bytes to the UI activity
-                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
-                            .sendToTarget();
-                } catch (IOException e) {
-                    break;
-                }
-            }
-        }
-
-        /* Call this from the main activity to shutdown the connection */
-        public void cancel() {
-            try {
-                socket.close();
-            } catch (IOException e) { }
-        }
-    }
-
 
 }
-
-
